a454bfaa4f65001d04cccf00ea9831900cc73d9c
Arup Patra <aruppatra2004@gmail.com>
2025-05-18 02:07:40 +0530

    necessary files added

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import dotenv from 'dotenv'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+   // vite config
+    define: {
+      'process.env.VITE_SERVICE_ID': JSON.stringify(process.env.VITE_SERVICE_ID),
+      'process.env.VITE_TEMPLATE_ID':  JSON.stringify(process.env.VITE_TEMPLATE_ID),
+      'process.env.VITE_PUBLIC_KEY': JSON.stringify(process.env.VITE_PUBLIC_KEY)
+    },
 })
